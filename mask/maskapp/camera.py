4d26f50935f3daf6bd93745f52671b3c5d6def37
mafiozzi1 <Mafia112>
2021-11-25 22:57:08 +0500

    1

--- mask/maskapp/camera.py
+++ mask/maskapp/camera.py
@@ -1,16 +1,24 @@
 import cv2
 
-from maskapp.yolo_model import YoloModel
+from maskapp.yolo_model import YoloModel, Singleton
 
-#
-def gen(camera):
+
+class WebCamera():
     model = YoloModel().model
-    while True:
-        ret, frame = camera.read()
-        results = model(frame, size=320)
-        results.render()
-        ret, jpeg = cv2.imencode('.jpg', results.imgs[0])
-        frame = jpeg.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    def __init__(self, camera=None):
+        self.__camera = camera
+
+    def get_camera(self):
+        return self.__camera
+
+    def gen(self):
+        while self.__camera is not None:
+            ret, frame = self.__camera.read()
+            results = self.model(frame, size=320)
+            results.render()
+            ret, jpeg = cv2.imencode('.jpg', results.imgs[0])
+            frame = jpeg.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
--- mask/maskapp/views.py
+++ mask/maskapp/views.py
@@ -9,17 +9,23 @@ import argparse
 import io
 import torch
 from maskapp.yolo_model import YoloModel
-from maskapp.camera import gen
+from maskapp.camera import WebCamera
 
 # Create your views here.
+cam = WebCamera(cv2.VideoCapture(0))
 
 
 def index(request):
+    global cam
+    if cam.get_camera():
+        cam.get_camera().release()
     return render(request, 'maskapp/about.html', {'name': 2})
 
 
 def mask_img(request):
-
+    global cam
+    if cam.get_camera():
+        cam.get_camera().release()
     if request.method == "POST" and request.FILES['file']:
 
         model = YoloModel().model  # Обученная модель
@@ -33,7 +39,8 @@ def mask_img(request):
         results = model(img, size=320)
         results.render()
 
-        pics = [os.path.join('./maskapp/static/maskapp/uploadpic/', x) for x in os.listdir('./maskapp/static/maskapp/uploadpic/') if x[-3:] == "jpg"]
+        pics = [os.path.join('./maskapp/static/maskapp/uploadpic/', x) for x in
+                os.listdir('./maskapp/static/maskapp/uploadpic/') if x[-3:] == "jpg"]
         pics.sort(key=lambda x: int(x.split('image')[1].split('.')[0]))
         path = os.path.abspath("./maskapp/static/maskapp/uploadpic/image" + str(len(pics)) + ".jpg")
         static_path = "/static/maskapp/uploadpic/image" + str(len(pics)) + ".jpg"
@@ -44,7 +51,6 @@ def mask_img(request):
             img_base64.save(path, format="JPEG")
         return render(request, 'maskapp/image.html', {'path': static_path})
 
-
     return render(request, 'maskapp/image.html')
 
 
@@ -53,7 +59,12 @@ def mask_vid(request):
 
 
 def video_feed(request):
-    cam = cv2.VideoCapture(0)
-    
-    return StreamingHttpResponse(gen(cam), content_type='multipart/x-mixed-replace; boundary=frame')
- 
+    global cam
+    cam = WebCamera(cv2.VideoCapture(0))
+    webcam = cam.get_camera()
+
+    if not (webcam is None or not webcam.isOpened()):
+        return StreamingHttpResponse(cam.gen(), content_type='multipart/x-mixed-replace; boundary=frame')
+    else:
+
+        return HttpResponse()
